bfc71039a1034b378c25b2b1873be4ea07d55fea
antgit <iljinaa@gmail.com>
2017-05-30 21:55:42 +0300

    script

---
+++ install_all.ps1
@@ -0,0 +1,13 @@
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+cinst skype -y
+cinst vlc -y
+choco install googlechrome -y
+cinst conemu -y
+cinst totalcommander  -y
+cinst steam -y
+cinst telegram -y
+
+#Development
+cinst git -y
+cinst tortoisegit -y
