f69fc05f5ff19698b4a6997e4d6abc37163f0347
ant <antgit@users.noreply.github.com>
2017-09-18 09:16:02 -0400

    Home category

--- install_all.ps1
+++ install_all.ps1
@@ -5,16 +5,17 @@ cinst vlc -y
 cinst googlechrome -y
 cinst conemu -y
 cinst totalcommander  -y
-cinst steam -y
 cinst telegram -y
 cinst notepadplusplus.install -y
-cinst transmission-qt -y
 cinst qtranslate -y
-cinst hearthstone-deck-tracker -y
-cinst spotify -y
 
 #Development
 cinst git -y
 cinst tortoisegit -y
 cinst nodejs.install -y
 cinst visualstudiocode -y
+
+#Home
+cinst hearthstone-deck-tracker -y
+cinst steam -y
+cinst transmission-qt -y
